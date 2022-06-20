9c63405906b5fe4b3c7174f6fdaf0fc5c6e42e62
thiagomeneguzzi <thiagonmeneguzzi@gmail.com>
2022-06-20 17:03:55 -0300

    tests on photo frame component

--- src/app/shared/components/photo-frame/photo-frame.component.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -1,25 +1,32 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators'
+import { debounceTime, takeUntil } from 'rxjs/operators'
 
 @Component({
     selector: 'app-photo-frame',
     templateUrl: './photo-frame.component.html',
     styleUrls: ['photo-frame.component.scss']
 })
-export class PhotoFrameComponent implements OnInit {
-
+export class PhotoFrameComponent implements OnInit, OnDestroy {
+    
     @Output() public liked: EventEmitter<void> = new EventEmitter();
     @Input() public description: string = '';
     @Input() public src: string = '';
     @Input() public likes: number = 0;
     private debounceSubject: Subject<void> = new Subject();
+    private unsubscribe: Subject<void> = new Subject();
 
     public ngOnInit(): void {
         this.debounceSubject
             .asObservable()
             .pipe(debounceTime(500))
+            .pipe(takeUntil(this.unsubscribe))
             .subscribe(() => this.liked.emit())
+    }
+
+    public ngOnDestroy(): void {
+        this.unsubscribe.next();
+        this.unsubscribe.complete();
     }
 
     
